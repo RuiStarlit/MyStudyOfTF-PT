7aed558f8103b554b0287224854e5c708a52d264
RuiStarlit <2653323282@qq.com>
2021-06-13 20:11:10 +0800

    fisrt commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# -*- coding: UTF-8 -*-
+"""
+written by Rui
+file:main.py
+create time:2021/06/13
+"""
+import tensorflow as tf
+import numpy as np
+
+print('Hello TensorFlow! \n')
+print('But I didn\'t learn Tensor Anlysis ' )
