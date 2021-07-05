fdb44e9c54208d85eebb7a7ff7f68deffc51ed92
RuiStarlit <2653323282@qq.com>
2021-07-05 15:23:34 +0800

    n

---
+++ LearningPyTorch/main.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+"""
+Writer: RuiStarlit
+File: main
+Project: LearningPyTorch
+Create Time: 2021-07-05
+
+"""
+
+import torch
+print(torch.__version__)
---
+++ LearningTF/CNN.py
@@ -0,0 +1,89 @@
+# -*- coding: UTF-8 -*-
+"""
+written by Rui
+file:CNN.py
+create time:2021/06/13
+"""
+import tensorflow as tf
+import numpy as np
+
+# 利用CNN模型对MNIST数据集训练
+
+class MNISTLoader:
+    def __init__(self):
+        minst = tf.keras.datasets.mnist
+        (self.train_data, self.train_label), (self.test_data, self.test_label) \
+            = minst.load_data()
+        # MNIST中的图像默认为uint8（0-255的数字）。以下代码将其归一化到0-1之间的浮点数，并在最后增加一维作为颜色通道
+        # 在 TensorFlow 中，图像数据集的一种典型表示是 [图像数目，长，宽，色彩通道数] 的四维张量。
+        self.train_data = np.expand_dims(self.train_data.astype(np.float32) / 255.0, axis=-1)  # [60000, 28, 28, 1]
+        self.test_data = np.expand_dims(self.test_data.astype(np.float32) / 255.0, axis=-1)  # [10000, 28, 28, 1]
+        self.train_label = self.train_label.astype(np.int32)  # [60000]
+        self.test_label = self.test_label.astype(np.int32)  # [10000]
+        self.num_train_data, self.num_test_data = self.train_data.shape[0], self.test_data.shape[0]
+
+    def get_batch(self, batch_size):
+        # 从数据集中随机取出batch_size个元素并返回
+        index = np.random.randint(0, self.num_train_data, batch_size)
+        return self.train_data[index, :], self.train_label[index]
+
+class CNN(tf.keras.Model):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = tf.keras.layers.Conv2D(
+            filters=32,             # 卷积层神经元（卷积核）数目
+            kernel_size=[5, 5],     # 感受野大小
+            padding='same',         # padding策略（vaild 或 same） 'same'为了保证输出与输入尺寸一致
+            activation=tf.nn.relu   # 激活函数
+        )
+        # 池化层
+        self.pool1 = tf.keras.layers.MaxPool2D(pool_size=[2, 2], strides=2)
+        self.conv2 = tf.keras.layers.Conv2D(
+            filters=64,
+            kernel_size=[5, 5],
+            padding='same',
+            activation=tf.nn.relu
+        )
+        self.pool2 = tf.keras.layers.MaxPool2D(pool_size=[2, 2], strides=2)
+        self.flatten = tf.keras.layers.Reshape(target_shape=(7 * 7 * 64,))
+        self.dense1 = tf.keras.layers.Dense(units=1024, activation=tf.nn.relu)
+        self.dense2 = tf.keras.layers.Dense(units=10)
+
+    def call(self, inputs):
+        x = self.conv1(inputs)                  # [batch_size, 28, 28, 32]
+        x = self.pool1(x)                       # [batch_size, 14, 14, 32]
+        x = self.conv2(x)                       # [batch_size, 14, 14, 64]
+        x = self.pool2(x)                       # [batch_size, 7, 7, 64]
+        x = self.flatten(x)                     # [batch_size, 7 * 7 * 64]
+        x = self.dense1(x)                      # [batch_size, 1024]
+        x = self.dense2(x)                      # [batch_size, 10]
+        output = tf.nn.softmax(x)
+        return output
+
+num_epochs = 5
+batch_size = 50
+learning_rate = 0.001
+model = CNN()
+data_loader = MNISTLoader()
+# 使用Adam优化器(自适应矩估计）
+optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+num_batches = int(data_loader.num_train_data // batch_size * num_epochs)
+for batch_index in range(num_batches):
+    X, y = data_loader.get_batch(batch_size)
+    with tf.GradientTape() as tape:
+        y_pred = model(X)
+        # 交叉熵损失函数
+        loss = tf.keras.losses.sparse_categorical_crossentropy(y_true=y, y_pred=y_pred)
+        loss = tf.reduce_mean(loss)
+        print("batch %d: loss %f" % (batch_index, loss.numpy()))
+    grads = tape.gradient(loss, model.variables)
+    optimizer.apply_gradients(grads_and_vars=zip(grads, model.variables))
+
+sparse_categorical_accuracy = tf.keras.metrics.SparseCategoricalAccuracy()
+num_batches = int(data_loader.num_test_data // batch_size)
+for batch_index in range(num_batches):
+    start_index, end_index = batch_index * batch_size, (batch_index + 1) * batch_size
+    y_pred = model.predict(data_loader.test_data[start_index: end_index])
+    sparse_categorical_accuracy.update_state(y_true=data_loader.test_label[start_index: end_index], y_pred=y_pred)
+    # 返回的是正确命中的概率
+print("test accuracy: %f" % sparse_categorical_accuracy.result())
